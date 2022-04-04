569fd14c0f8b389e7bb8ea8a81fc8fb590b2b25a
needslyp <80572210+needslyp@users.noreply.github.com>
2022-04-04 21:44:02 +0300

    prog with parser

--- second_lab/main.py
+++ second_lab/main.py
@@ -1,24 +1,25 @@
 from datetime import datetime
-from train_base import TrainBase, CustomContentHandler
-import xml.sax
-
-def main():
-    handler = CustomContentHandler()
-    xml.sax.parse('myfile.xml', handler)
-    print(f'There were {handler.postCount} post elements')
-    print(f'There were {handler.entryCount} entry elements')
+from train_base import TrainBase, CustomContentHandler, ParserToFile
+import xml.sax, xml.dom.minidom
 
 
-base = TrainBase()
-base.add_title('2','12','13', datetime(10,10,10,10,10,10), datetime(402,11,11,11,11,11))
-base.add_title('1','12','13', datetime(10,10,10,10,10,10), datetime(403,1,29,11,11,11))
-base.add_title('2','12','13', datetime(10,10,10,10,10,10), datetime(404,1,29,11,11,11))
-base.add_title('2','12','13', datetime(10,10,10,10,10,10), datetime(405,1,29,11,11,11))
-base.add_title('1','12','13', datetime(10,10,10,10,10,10), datetime(406,1,29,11,11,11))
+handler = CustomContentHandler()
+ptf = ParserToFile()
+train_base = TrainBase()
 
-base.print_all()
-base.del_title_by_numb('2')
-base.print_all()
+train_base.set_from_file(handler, "test_minidom.xml")
 
-if __name__ == '__main__':
-    main()
+train_base.print_all()
+
+train_base.add_title('2','12','13', datetime(10,10,10,10,10,10), datetime(402,11,11,11,11,11))
+train_base.add_title('19','12','13', datetime(10,10,10,10,10,10), datetime(403,1,29,11,11,11))
+train_base.add_title('12','12','13', datetime(10,10,10,10,10,10), datetime(404,1,29,11,11,11))
+train_base.add_title('7','12','13', datetime(10,10,10,10,10,10), datetime(405,1,29,11,11,11))
+train_base.add_title('4','12','13', datetime(10,10,10,10,10,10), datetime(423,1,29,11,11,11))
+
+train_base.print_all()
+print("Trains ", train_base.size)
+
+ptf.push_inf(train_base, "test_minidom.xml")
+
+print("Trains ", train_base.size)
