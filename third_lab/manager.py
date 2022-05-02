f0537aed43b710e5d25108376f92e959bbde1d5c
needslyp <80572210+needslyp@users.noreply.github.com>
2022-05-02 11:55:05 +0300

    normal movement fow WASD and new WEAPONS

--- third_lab/manager.py
+++ third_lab/manager.py
@@ -1,12 +1,7 @@
-
 class ScoreManager:
     score = 0
 
 
 class Background:
-    max_x = 250
-    min_x = -250
-    max_y = 250
-    min_y = -250
     display_scroll = [0, 0]
 
--- third_lab/toolbox.py
+++ third_lab/toolbox.py
@@ -18,7 +18,6 @@ def getRotatedImage(image, rect, angle):
     new_rect = new_image.get_rect(center=rect.center)
     return new_image, new_rect
 
-
 def angleBetweenPoints (x1, y1, x2, y2):
     x_difference = x2 - x1
     y_difference = y2 - y1
